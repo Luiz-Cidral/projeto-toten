69366c9bbe3a08d07831ec84731741b402f17979
Luiz-Cidral <cidral2905@gmail.com>
2026-07-07 10:28:57 -0300

    fix: rotas configuradas com hash history

--- ProjetoToten/vue/src/router/index.js
+++ ProjetoToten/vue/src/router/index.js
@@ -6,7 +6,7 @@ import SuccessView from '@/views/SuccessView.vue'
 
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
